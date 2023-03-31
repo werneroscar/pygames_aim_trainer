e914f2cefe0dbe8605d3a82476c31859bd25799d
Werner Oscar <wernertsekumah@gmail.com>
2023-03-31 23:22:55 +0000

    increased window width

--- aim_trainer.py
+++ aim_trainer.py
@@ -6,8 +6,8 @@ pygame.init()
 pygame.display.set_caption("Alien Aim Trainer")
 
 # Set up the game window
-window_width = 500
-window_height = 500
+window_width = 800
+window_height = 600
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Aim Trainer")
 
